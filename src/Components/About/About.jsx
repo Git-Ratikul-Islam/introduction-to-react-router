11781217b5779013ff46b43459f4e3909af0ff54
Ratikulislam <joyofficial80@gmail.com>
2023-09-19 19:57:39 +0600

    nested-router-created-and-outlet

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,12 @@
+
+
+const About = () => {
+      return (
+            <div>
+                  <h2>About us</h2>
+                  <p>Everything you wanted to know</p>
+            </div>
+      );
+};
+
+export default About;
